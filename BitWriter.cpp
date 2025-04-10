1f70725546b1ffd545c19bb8aaf8f2d65478de3c
angelwing888 <kyliecchang@gmail.com>
2025-04-09 18:07:59 -0700

    setup

---
+++ BitWriter.cpp
@@ -0,0 +1,14 @@
+//
+// Created by kchang37 on 4/9/25.
+//
+class BitWriter {
+
+    void write(bool bitValue) {
+
+    }
+
+    size_t getData(vector<uint8_t> &out) {
+
+    }
+
+};
---
+++ main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by kchang37 on 4/9/25.
+//
+
+int main() {
+    return 0;
+}
