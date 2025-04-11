68695509252c3397b8653cf80a45dcaa399a5603
angelwing888 <kyliecchang@gmail.com>
2025-04-10 21:25:31 -0700

    done

--- BitWriter.cpp
+++ BitWriter.cpp
@@ -1,14 +1,60 @@
-//
-// Created by kchang37 on 4/9/25.
-//
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
 class BitWriter {
+    private:
+    std::vector<uint8_t> bitSet;
+    // keeps track of how many bits out of 8 have been written
+    int position = 0;
+    uint8_t byte = 0;
+    size_t bitCount = 0;
 
+    public:
     void write(bool bitValue) {
+        // left shift by one place
+        byte <<= 1;
+        // if true, sets the value to 1
+        // otherwise, nothing is needed
+        if (bitValue) {
+            byte |= 1;
+        }
+        ++bitCount;
+        ++position;
 
+        // pushes full byte to vector and resets position back to 0
+        if (position == 8) {
+            bitSet.push_back(byte);
+            byte = 0;
+            position = 0;
+        }
     }
 
-    size_t getData(vector<uint8_t> &out) {
+    size_t getData(std::vector<uint8_t> &out) {
+        // copy over the data currently in bitSet
+        out = bitSet;
+        // completes byte by placing 0s if incomplete
+        if (position > 0) {
+            out.push_back(byte);
+        }
+        return bitCount;
+    }
 
+    // referenced https://stackoverflow.com/questions/69563813/convert-stdarray-of-bytes-to-hex-stdstring
+    // and had to look up stringstreams bc i've never seen those before... or any of this
+    // and iomanip https://cplusplus.com/reference/iomanip/
+    std::string toHexString(std::vector<uint8_t> bytes, const std::string& delim = "") {
+        std::stringstream output;
+        for (size_t i = 0; i < bytes.size(); ++i) {
+            output << std::uppercase << std::hex << std::setw(2) << std::setfill('0') << (int)(bytes[i]);
+            if (i != bytes.size() - 1) {
+              output << delim;
+            }
+        }
+        return output.str();
     }
 
 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,119 @@
-//
-// Created by kchang37 on 4/9/25.
-//
+#include "BitWriter.cpp"
 
 int main() {
+
+    int passed = 0;
+    int totalTests = 5;
+
+    BitWriter writer;
+    std::vector<bool> bits = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
+    for (bool b : bits) {
+        writer.write(b);
+    }
+
+    std::vector<u_int8_t> outputs;
+    writer.getData(outputs);
+
+    // example test
+    std::cout << "Data set: {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0}" << std::endl;
+    // TEST CASE 1
+    std::cout << "Testing hex outputs..." << std::endl;
+    std::string actualHex = writer.toHexString(outputs, "-");
+    std::string expectedHex = "AA-0A";
+    if (actualHex == expectedHex) {
+        std::cout << "Test passed." << std::endl;
+        ++passed;
+    }
+    else {
+        std::cout << "Test failed." << std::endl;
+        std::cout << "Expected: " << expectedHex << std::endl;
+        std::cout << "Actual: " << actualHex << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    // TEST CASE 2
+    size_t size = writer.getData(outputs);
+    std::cout << "Testing size... " << std::endl;
+    if (size == 12) {
+        passed++;
+        std::cout << "Test passed." << std::endl;
+    }
+    else {
+        std::cout << "Test failed." << std::endl;
+        std::cout << "Expected: 12" << std::endl;
+        std::cout << "Actual: " << size << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    // testing 4-bit entry
+    std::cout << "Data set: {1, 0, 0, 1}" << std::endl;
+    BitWriter writer2;
+    std::vector<bool> bits2 = {1, 0, 0, 1};
+    for (bool b : bits2) {
+        writer2.write(b);
+    }
+
+    std::vector<u_int8_t> outputs2;
+    writer2.getData(outputs2);
+
+    // TEST CASE 3
+    std::cout << "Testing hex outputs..." << std::endl;
+    std::string actualHex2 = writer2.toHexString(outputs2, "-");
+    std::string expectedHex2 = "09";
+    if (actualHex2 == expectedHex2) {
+        std::cout << "Test passed." << std::endl;
+        ++passed;
+    }
+    else {
+        std::cout << "Test failed." << std::endl;
+        std::cout << "Expected: " << expectedHex2 << std::endl;
+        std::cout << "Actual: " << actualHex2 << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    // TEST CASE 4
+    size_t size2 = writer2.getData(outputs);
+    std::cout << "Testing size... " << std::endl;
+    if (size2 == 4) {
+        ++passed;
+        std::cout << "Test passed." << std::endl;
+    }
+    else {
+        std::cout << "Test failed." << std::endl;
+        std::cout << "Expected: 4" << std::endl;
+        std::cout << "Actual: " << size2 << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Data set: {}" << std::endl;
+    // TEST CASE 5 (empty list)
+    BitWriter writer3;
+    std::vector<bool> bits3;
+    std::cout << "Testing empty list... " << std::endl;
+    std::vector<u_int8_t> empty;
+    writer3.getData(empty);
+    std::string actualHex3 = writer3.toHexString(empty, "-");
+    std::string expectedHex3 = "";
+    if (actualHex3 == expectedHex3) {
+        std::cout << "Test passed." << std::endl;
+        ++passed;
+    }
+    else {
+        std::cout << "Test failed." << std::endl;
+        std::cout << "Expected: " << expectedHex3 << std::endl;
+        std::cout << "Actual: " << actualHex3 << std::endl;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Total tests passed: " << passed << std::endl;
+    std::cout << "Total tests failed: " << (totalTests - passed) << std::endl;
+
+
+
+
     return 0;
 }
